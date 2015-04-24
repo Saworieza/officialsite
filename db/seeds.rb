6cf1903a13c5e6e3fd20cb5110c484bc85165e7e
sigu <sigumagwa@gmail.com>
2015-04-24 09:21:04 +0300

    started on show members. added foundation navigation links

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Member.create(first_name: "Myname", last_name: "OtherName", email: "mail@first.co", mobile: "34232445")
+Member.create(first_name: "Fourth", last_name: "OtherName", email: "mail4@first.co", mobile: "434232445")
+Member.create(first_name: "fifth", last_name: "OtherName", email: "mail5@first.co", mobile: "534232445")
+Member.create(first_name: "sixth", last_name: "name", email: "mail6@first.co", mobile: "634232445")
+Member.create(first_name: "out", last_name: "worl", email: "mail7@first.co", mobile: "734232445")
+Member.create(first_name: "veryvery", last_name: "loong", email: "mail8@first.co", mobile: "834232445")
--- spec/views/members/index.html.erb_spec.rb
+++ spec/views/members/index.html.erb_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe "members/index", type: :view do
   before(:each) do
     assign(:members, [
       Member.create!(
-        :first_name => "First Name",
-        :last_name => "Last Name",
+        :first_name => "First",
+        :last_name => "Last",
         :email => "my@email.com",
         :mobile => "Mobile",
         :password => "Mysringpass"
       ),
       Member.create!(
-        :first_name => "First Name",
-        :last_name => "Last Name",
+        :first_name => "First",
+        :last_name => "Last",
         :email => "my1@email.com",
         :mobile => "Mobile",
         :password => "Mysringpass"
@@ -22,8 +22,9 @@ RSpec.describe "members/index", type: :view do
 
   it "renders a list of members" do
     render
-    assert_select "tr>td", :text => "First Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Last Name".to_s, :count => 2
+    assert_select "h1", :text => "LakeHub Members".to_s, :count => 1
+    assert_select ".panel>h6", :text => "Name: First  Last".to_s, :count => 2
+    assert_select ".button", :text => "Detailed Profile".to_s, :count => 2
     # assert_select "tr>td", :text => "Email".to_s, :count => 2
     # assert_select "tr>td", :text => "Mobile".to_s, :count => 2
   end
