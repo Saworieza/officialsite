c137faeeb9f7e698473243a7d60dd7e61000e407
sigu <sigumagwa@gmail.com>
2015-04-25 10:36:10 +0300

    reintroduce devise to handle members

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,10 +1,10 @@
 class Member < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, #:registerable,
-         :recoverable, :rememberable, :trackable#, :validatable
-	validates :first_name, :last_name, :email, :mobile , presence: true
-	EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, length: {maximum: 255}, uniqueness: {case_sensitive: false},
-						format: {with: EMAIL_REGEX}
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable#, :confirmable
+	# validates :first_name, :last_name, :email, :mobile , presence: true
+	# EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	# validates :email, length: {maximum: 255}, uniqueness: {case_sensitive: false},
+	# 					format: {with: EMAIL_REGEX}
 end
