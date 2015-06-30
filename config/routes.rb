a46484421b8c495f5d1dfa4ceb625941242b76bb
sigu <sigumagwa@gmail.com>
2015-06-30 10:25:54 +0300

    generate static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
   devise_for :members
   resources :members, :only => ['show', 'index']
 
